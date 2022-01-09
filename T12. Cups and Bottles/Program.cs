43e58d9e0829fe437300ad40134c0eb579e09718
Storm603 <93555141+Storm603@users.noreply.github.com>
2022-01-09 23:13:51 +0200

    Add files via upload

---
+++ T12. Cups and Bottles/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Transactions;
+
+namespace T12._Cups_and_Bottles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] cupsCapacities = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] bottlesWithWatter = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Queue<int> cups = new Queue<int>(cupsCapacities);
+            Stack<int> bottles = new Stack<int>(bottlesWithWatter);
+            
+            int wastedWater = 0;
+            int currCup = 0;
+
+            while (cups.Any() && bottles.Any())
+            {
+                if (currCup <= 0)
+                {
+                    currCup = cups.Peek();
+                }
+                int currBottle = bottles.Pop();
+
+
+                if (currCup > currBottle)
+                {
+                    currCup -= currBottle;
+                }
+                else if (currBottle >= currCup)
+                {
+                    currCup -= currBottle;
+                    wastedWater += currCup;
+
+                    cups.Dequeue();
+                }
+            }
+
+
+            if (cups.Any())
+            {
+                Console.WriteLine($"Cups: {string.Join(" ", cups)}");
+            }
+            else
+            {
+                Console.WriteLine($"Bottles: {string.Join(" ", bottles)}");
+            }
+
+            Console.WriteLine($"Wasted litters of water: {Math.Abs(wastedWater)}");
+        }
+    }
+}
